4ae3fb16539fbf7f1f57980103c77c10b7670327
Dan Roddis <danroddis@gmail.com>
2022-05-14 15:38:26 +0100

    Move hash parameters out of methods

--- HashidService/Program.cs
+++ HashidService/Program.cs
@@ -1,4 +1,5 @@
 using HashidsNet;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,16 +13,19 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+var alphabet = "0123456789ABCDEGHJKLMPQRSTUVWXYZ";
+var minHashLength = 6;
+
 app.MapGet("/encode", (int id, string salt) =>
 {
-    var hashids = new Hashids(salt, 6, "0123456789ABCDEGHJKLMPQRSTUVWXYZ");
+    var hashids = new Hashids(salt, minHashLength, alphabet);
     var hash = hashids.Encode(id);
     return hash;
 });
 
 app.MapGet("/decode", (string hash, string salt) =>
 {
-    var hashids = new Hashids(salt, 6, "0123456789ABCDEGHJKLMPQRSTUVWXYZ");
+    var hashids = new Hashids(salt, minHashLength, alphabet);
     var id = hashids.Decode(hash);
     return id;
 });
